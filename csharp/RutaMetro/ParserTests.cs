e422435ad6ab4b7780223746e69dea7bcdb6607f
Alfredo Chavez <achavez@reachcore.com>
2017-08-28 13:26:04 -0500

    Quitamos elementos superfluos y probamos que podemos deserializar el archivo en objetos.

---
+++ csharp/RutaMetro/ParserTests.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeCamp.RutaMetro
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Serialization;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ParserTests
+    {
+        // La ruta es relativa a partir del directorio "bin" del proyecto.
+        private const string KlmPath = @"..\..\..\..\Metro_CDMX.kml.xml";
+
+        [Test]
+        public void ReadModelFrom()
+        {
+            Kml model = null;
+
+            using (var txtReader = new StreamReader(KlmPath))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(Kml), Xmlns.Klm);
+                model = (Kml)xmlSerializer.Deserialize(txtReader);
+            }
+
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model.Document.Folders.Count, Is.EqualTo(3));
+
+            var lineas = model.Document.Folders[0];
+            Assert.That(lineas.Name, Is.EqualTo("Líneas de Metro"));
+            Assert.That(lineas.Placemarks.Count, Is.EqualTo(12));
+
+            var estaciones = model.Document.Folders[1];
+            Assert.That(estaciones.Name, Is.EqualTo("Estaciones de Metro"));
+            Assert.That(estaciones.Placemarks.Count, Is.EqualTo(162));
+        }
+    }
+}
